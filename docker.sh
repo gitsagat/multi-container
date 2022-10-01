95fc16548989542ca0f2ad2efbbd64e9d26e5a8a
gitsagat <git@sagat.com.br>
2022-09-30 22:49:30 -0300

    Commit initial

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 echo "Install Docker and docker compose and nginx.."
 
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt-get --yes --force-yes install apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
@@ -11,10 +11,13 @@ echo \
 
 sleep 5
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo apt-get install docker-compose
+sudo apt-get --yes --force-yes install docker-ce docker-ce-cli containerd.io
+sudo apt-get --yes --force-yes install docker-compose
 sudo service docker start
 
-sudo apt-get install nginx -y
+sudo apt-get --yes --force-yes install nginx -y
 sudo nginx -t
 sudo systemctl restart nginx
+
+sudo apt-get update
+sudo apt-get --yes --force-yes install certbot python3-certbot-nginx
